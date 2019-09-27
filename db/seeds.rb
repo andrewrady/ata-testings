64788bfcbd8780d579da211dd9f15befb322c82f
Andrew Rady <andrew.arsoftware@gmail.com>
2019-09-27 13:12:22 -0600

    updated seed to include users

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,14 @@ require 'faker'
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create([{ email: 'test1@gmail.com', password: 'passowrd'}, { email: 'test2@gmail.com', password: 'password'}])
+
 25.times do
   first_name = Faker::Name.first_name
   last_name = Faker::Name.last_name
   rank = "White"
   size = 4
+  user_id = User.first.id
 
-  Student.create(firstName: first_name, lastName: last_name, rank: rank, size: size)
+  Student.create(firstName: first_name, lastName: last_name, rank: rank, size: size, user_id: user_id)
 end
