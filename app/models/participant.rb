c126c0a220e3391d05a1d050a941937ba046d2a6
Andrew Rady <andrew.arsoftware@gmail.com>
2019-09-30 19:25:51 -0600

    adds participant tests

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -4,9 +4,9 @@ class Participant < ApplicationRecord
 
   serialize :categories
 
-  validates :firstName, presense: true
-  validates :lastName, presense: true
-  validates :categories, presense: true
-  validates :testing_id, presense: true
-  validates :student_id, presense: true
+  validates :firstName, presence: true
+  validates :lastName, presence: true
+  validates :categories, presence: true
+  validates :testing_id, presence: true
+  validates :student_id, presence: true
 end
--- spec/models/participant_spec.rb
+++ spec/models/participant_spec.rb
@@ -8,7 +8,39 @@ RSpec.describe Participant, type: :model do
     it 'ensures first name presense' do
       participant = Participant.new
       participant.lastName = 'Last'
-      participant
+      participant.valid?
+      expect(participant.errors[:firstName]).to include("can't be blank")
+    end
+
+    it 'ensures last name presense' do
+      participant = Participant.new
+      participant.firstName = 'First'
+      participant.valid?
+      expect(participant.errors[:lastName]).to include("can't be blank")
+    end
+
+    it 'ensures categories presense' do
+      participant = Participant.new
+      participant.firstName = 'First'
+      participant.lastName = 'Last'
+      participant.valid?
+      expect(participant.errors[:categories]).to include("can't be blank")
+    end
+
+    it 'ensures categories presense' do
+      participant = Participant.new
+      participant.firstName = 'First'
+      participant.lastName = 'Last'
+      participant.valid?
+      expect(participant.errors[:testing_id]).to include("can't be blank")
+    end
+
+    it 'ensures categories presense' do
+      participant = Participant.new
+      participant.firstName = 'First'
+      participant.lastName = 'Last'
+      participant.valid?
+      expect(participant.errors[:student_id]).to include("can't be blank")
     end
   end
 end
