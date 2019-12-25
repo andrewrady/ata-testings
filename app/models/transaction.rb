8346ac6764df4fbacdfeb4ec70a0288754efc830
Abdrew Rady <andrew.arsoftware@gmail.com>
2019-12-24 19:12:02 -0700

    adds validation for internal data in model for transactions

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,8 @@
 class Transaction < ApplicationRecord
-  has_many: transaction_item
+  belongs_to :student
+  has_many :transaction_item
+
+  validates :total, presence: true
+  validates :tax, presence: true
+  validates :discount, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191224032246) do
+ActiveRecord::Schema.define(version: 20191224034308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,6 +116,8 @@ ActiveRecord::Schema.define(version: 20191224032246) do
     t.string "orderId"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "student_id"
+    t.index ["student_id"], name: "index_transactions_on_student_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -132,4 +134,5 @@ ActiveRecord::Schema.define(version: 20191224032246) do
 
   add_foreign_key "ranks", "students"
   add_foreign_key "transaction_items", "transactions", column: "transactions_id"
+  add_foreign_key "transactions", "students"
 end
