c2930660c96683e8c30403670f39b6df5f7f0cc0
Andrew Rady <andrew.arsoftware@gmail.com>
2020-01-26 15:26:39 -0700

    adds js to make mobile menu work and styling

--- app/assets/javascripts/nav-header.js
+++ app/assets/javascripts/nav-header.js
@@ -2,10 +2,19 @@ document.addEventListener('turbolinks:load', function () {
   const navIcon = document.querySelector('.navbar-toggler');
   const menu = document.querySelector('.collapse');
   if(navIcon) {
-    navIcon.addEventListener('click', function(e) {
-      menu.classList.toggle('show')
-      console.log(menu.classList)
+    navIcon.addEventListener('click', () => {
+      menu.classList.toggle('show');
+    })
+  }
 
+  const nestedMenu = document.querySelectorAll('.dropdown-toggle');
+  if(nestedMenu.length) {
+    nestedMenu.forEach(menu => {
+      const parent = menu.parentNode;
+      const subMenu = Array.from(parent.children).find(x => Array.from(x.classList).includes('dropdown-menu'))
+      parent.addEventListener('click', () => {
+        subMenu.classList.toggle('d-none');
+      })
     })
   }
 });
