e0b223cfb2d26dd558dc389f9c6d8bb0a29f696e
Andrew Rady <andrew.arsoftware@gmail.com>
2020-01-26 15:48:47 -0700

    removed status from sort

--- app/controllers/testings_controller.rb
+++ app/controllers/testings_controller.rb
@@ -2,7 +2,7 @@ class TestingsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @testings = Testing.all.order(:status).order(created_at: :desc)
+    @testings = Testing.all.order(created_at: :desc)
   end
 
   def show
