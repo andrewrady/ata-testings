90dbe25346dd85ebb944c178e1faa136567bddf6
Andrew Rady <andrew.arsoftware@gmail.com>
2020-02-14 16:08:36 -0700

    basic scaffold for ui pos

--- app/javascript/pos.vue
+++ app/javascript/pos.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <h1>POS Terminal</h1>
     <div class="row">
-      <div class="col-9">
+      <div class="col-10">
       <form @submit.prevent>
         <div class="form-row">
           <div class="form-group d-flex">
@@ -11,18 +11,48 @@
           </div>
         </div>
       </form>
+      <div class="results d-flex my-2">
+        <div class="card" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Student Name</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Rank</h6>
+            <div class="card-text">
+              <p>9105 W Laguna Ct, Boise Idaho</p>
+              <p>9105 W Laguna Ct, Boise Idaho</p>
+            </div>
+            <a href="#" class="card-link" target="_blank">Profile</a>
+          </div>
+        </div>
+        <div class="card mx-2" style="width: 18rem;">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="card-link">Card link</a>
+          </div>
+        </div>
+      </div>
       <pos-item
         v-for="(item, index) in items"
         :key="index"
         :index="index"
         :item="item">
       </pos-item>
-      <button class="btn btn-primary" @click="items.push(itemTemplate)">Add Item</button>
+      <!-- <div class="d-flex justify-content-between">
+        <button class="btn btn-success">Charge</button>
+        <button class="btn btn-primary" @click="items.push(itemTemplate)">Add Item</button>
+      </div> -->
       </div>
-      <div class="col-3">
-        <div class="alert alert-info rounded-circle total">
+      <div class="col-2">
+        <div class="alert alert-info rounded-circle pos-button">
           <p>Total</p>
-          {{ total}}
+          {{ total | money }}
+        </div>
+        <div class="alert alert-success rounded-circle pos-button d-flex">
+          <h4 class="align-self-center">Charge</h4>
+        </div>
+        <div class="alert alert-primary rounded-circle pos-button d-flex" @click="items.push(itemTemplate)">
+          <h4 class="align-self-center">Add Item</h4>
         </div>
       </div>
     </div>
@@ -38,7 +68,7 @@ export default {
   },
   computed: {
     total() {
-      return 0
+      return 20
     }
   },
   data() {
@@ -53,6 +83,17 @@ export default {
   },
   mounted() {
     this.items.push(this.itemTemplate);
+  },
+  filters: {
+    money(value){
+      let test = value;
+      if(!test) 0;
+
+      return test.toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD'
+      })
+    }
   }
 }
 </script>
--- app/javascript/postItem.vue
+++ app/javascript/postItem.vue
@@ -2,11 +2,23 @@
   <div class="form-row">
     <div class="form-group col-md-5">
       <input type="text" class="form-control"  placeholder="name">
+      <template v-if="results.length">
+        <div class="card rounded-0">
+          <div class="card-body">
+            This is some text within a card body.
+          </div>
+        </div>
+        <div class="card rounded-0">
+          <div class="card-body">
+            This is some text within a card body.
+          </div>
+        </div>
+      </template>
     </div>
-    <div class="form-group col-md-2">
+    <div class="form-group col-md-3">
       <input type="text" class="form-control"  placeholder="price">
     </div>
-    <div class="form-group col-md-2">
+    <div class="form-group col-md-">
       <input type="text" class="form-control" placeholder="qunity">
     </div>
     <div class="form-group">
@@ -15,8 +27,8 @@
         <label class="form-check-label">tax</label>
       </div>
     </div>
-    <div class="form-group">
-      <i class="fas fa-times" v-if="index"></i>
+    <div class="form-group mx-3">
+      <i class="far fa-times-circle red close" v-if="index"></i>
     </div>
   </div>
 </template>
@@ -27,6 +39,11 @@ export default {
   props: {
     item: Object,
     index: Number
+  },
+  data() {
+    return {
+      results: []
+    }
   }
 }
 </script>
